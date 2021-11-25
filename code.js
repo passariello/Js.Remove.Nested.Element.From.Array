688892d16c3353b13417f3960348b95968f35c8a
Dario Passariello <dariopassariello@gmail.com>
2021-11-25 12:22:44 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,50 +1,27 @@
 // CREATED BY DARIO PASSARIELLO
-// Copiright 2020
+// Copiright 2021
 
 const DeleteElementFromArray = ( array, id ) =>{
   
     array.some(function iter (o,i,a) {
       
         if (o.Id === id) {
-            a.splice(i, 1)
-            return true
+            a.splice(i, 1) return true
         }
         
-        var key = Object.keys(o)      
-        for (let key of Object.keys(o)) {
-        let value = o[key];
+        var key = Object.keys(o)
+        
+        for (let key of Object.keys(o)) {        
+          let value = o[key]
           if(value?.length && typeof value === 'object') return value && value.map(iter)
         }
       
         // OLD SCHOOL WAY 
-        for (var p=0; p < key.length; ++p){
-          if( o[key[p]]?.length && typeof o[key[p]] === 'object') return o[key[p]] && o[key[p]].some(iter)
-        }      
+        // for (var p=0; p < key.length; ++p){
+        //  if( o[key[p]]?.length && typeof o[key[p]] === 'object') return o[key[p]] && o[key[p]].some(iter)
+        // }      
       
     })
     
 }
 
-/*
-usage:
-
-your_multidimensional_array =
-[
-    {
-        "Id": "65affc02-834f-41dc-b4cf-0f4200d8a533",
-        "test": "7eebd408-4aef-4a71-b96c-0151de3d41f4",
-        "NestedArray": [
-            {
-                "Id": "1cc27e18-1cb6-4e93-893b-a018fc134ed8",
-                "test": "69967e2d-b44e-431e-a012-9b18126ed34b",
-            }
-        ]
-    }
-]
-
-your_uuid = 69967e2d-b44e-431e-a012-9b18126ed34b
-
-Example:
-
-DeleteElementFromArray( your_multidimensional_array , your_uuid )
-*/
