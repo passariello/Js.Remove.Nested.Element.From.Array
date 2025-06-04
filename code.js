0dec3b20d36e7f733325a6ea3c92a982b56236b7
Dario Passariello <dariopassariello@gmail.com>
2025-06-03 19:29:01 -0600

    update

--- code.js
+++ code.js
@@ -5,24 +5,25 @@
 
 const DeleteElementFromArray = ( array, id ) =>{
   
-    array.some(function iter (o,i,a) {
+    array.some(
+        function iter (o,i,a) {
       
-        if (o.Id === id) {
-            a.splice(i, 1) return true
-        }
+            if (o.Id === id) {
+                a.splice(i, 1); 
+                return true;
+            }
+                
+            for (let key of Object.keys(o)) {        
+            let value = o[key];
+            if(value?.length && typeof value === 'object') return value && value.map(iter);
+            }
         
-        var key = Object.keys(o)
-       
-        for (let key of Object.keys(o)) {        
-          let value = o[key]
-          if(value?.length && typeof value === 'object') return value && value.map(iter)
+            // OLD SCHOOL WAY 
+            // for (var p=0; p < key.length; ++p){
+            //  if( o[key[p]]?.length && typeof o[key[p]] === 'object') return o[key[p]] && o[key[p]].some(iter)
+            // }      
+        
         }
-      
-        // OLD SCHOOL WAY 
-        // for (var p=0; p < key.length; ++p){
-        //  if( o[key[p]]?.length && typeof o[key[p]] === 'object') return o[key[p]] && o[key[p]].some(iter)
-        // }      
-      
-    })
+    )
     
 }
